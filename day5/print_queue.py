186b121de5b932dd73bc2abadff6345070b5a9e8
wangwiza <76924804+wangwiza@users.noreply.github.com>
2024-12-05 08:37:25 -0500

    day 5 part 1 complete

---
+++ day5/print_queue.py
@@ -0,0 +1,40 @@
+def print_queue():
+
+    page_ordering = {}
+    updates = []
+
+    with open('input.txt') as f:
+        # parse page ordering rules
+        for line in f:
+            if line == '\n':
+                break
+            x, y = line.strip().split('|')
+            x, y = int(x), int(y)
+            if x not in page_ordering:
+                page_ordering[x] = set([y])
+            else:
+                page_ordering[x].add(y)
+
+        # parse pages in updates
+        for line in f:
+            updates.append([int(x) for x in line.strip().split(',')])
+    
+    def verify_update(update):
+        for i in range(len(update)):
+            for j in range(i, len(update)):
+                if update[i] in page_ordering.get(update[j], []):
+                    return False
+        return True
+
+    ans = 0
+    # verify each update
+    for update in updates:
+        if verify_update(update):
+            ans += update[len(update)//2]
+    return ans
+
+
+
+
+if __name__ == "__main__":
+    print(print_queue())
