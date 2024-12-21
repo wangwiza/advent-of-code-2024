bd6dbcd53a37ad4a6178de99734f3328b3b7b6fe
wangwiza <76924804+wangwiza@users.noreply.github.com>
2024-12-21 06:53:00 -0500

    day 9 part 2 finally done

--- day9/disk_fragmenter.py
+++ day9/disk_fragmenter.py
@@ -1,30 +1,3 @@
-
-def disk_fragmenter_attempt():
-    with open('input.txt') as f:
-        disk_map = list(map(int, f.read().strip()))
-
-    front = 0
-    back = len(disk_map)-1
-    compact = ""
-    while front < back-1:
-        # print(front, back, disk_map)
-        if front % 2 == 0: # front is on file-block
-            compact += str(front//2)*disk_map[front] # append front file
-            front += 1
-        elif disk_map[front] >= disk_map[back]: # enough free space
-            compact += str(back//2)*disk_map[back] # append back file
-            disk_map[front] -= disk_map[back] # consumed front space
-            back -= 2 # go to next back file
-        else: # not enough free space
-            compact += str(back//2)*(disk_map[front])
-            disk_map[back] -= disk_map[front] # consumed back file
-            front += 1
-    compact += str(back//2)*disk_map[back]
-    checksum = 0
-    for i, v in enumerate(compact):
-        checksum += i*int(v)
-    return checksum
-
 def disk_fragmenter():
     with open('input.txt') as f:
         disk_map = list(map(int, f.read().strip()))
@@ -49,25 +22,36 @@ def disk_fragmenter():
 def disk_fragmenter2():
     with open('input.txt') as f:
         disk_map = list(map(int, f.read().strip()))
-    
-    expanded = []
+    disk_list = []
+    space_list = []
+    file_list = []
+    count = 0
     for i in range(len(disk_map)):
         if i % 2 == 0:
-            expanded += [str(i//2)]*disk_map[i]
+            disk_list += [str(i//2)]*disk_map[i]
+            file_list.append((str(i//2),disk_map[i],count))
         else:
-            expanded += ['.']*disk_map[i]
-    i = 0
-    while i < len(expanded):
-        if expanded[i] != '.':
-            i += 1
-            continue
-        while (v:=expanded.pop()) == '.':
-            pass
-        expanded[i] = v
-        i += 1
-    return sum(i*int(v) for i,v in enumerate(expanded))
+            disk_list += ['.']*disk_map[i]
+            space_list.append((disk_map[i], count))
+        count += disk_map[i]
+    file_list = file_list[::-1] # reverse to start from biggest value file
+    for file in file_list:
+        for i in range(len(space_list)):
+            if file[1] > space_list[i][0] or file[2] < space_list[i][1]:
+                continue
+            for j in range(space_list[i][1],space_list[i][1]+file[1]):
+                disk_list[j] = file[0]
+            for j in range(file[2],file[2]+file[1]):
+                disk_list[j] = '.'
+            if file[1] == space_list[i][0]:   
+                space_list.pop(i)
+            elif file[1] < space_list[i][0]:
+                space_list[i] = (space_list[i][0]-file[1],space_list[i][1]+file[1])
+            break
+    return sum(i*int(file_id) for i, file_id in enumerate(disk_list) if file_id != '.')
+            
 
 
 if __name__ == '__main__':
-    print(disk_fragmenter())
+    # print(disk_fragmenter())
     print(disk_fragmenter2())
